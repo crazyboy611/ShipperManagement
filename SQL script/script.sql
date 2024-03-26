9d51dcccca548759ade6020a5afbe34b4a0aeaab
crazyboy611 <phungductrung611@gmail.com>
2024-03-26 08:36:25 +0700

    Shipper Management SQL script

---
+++ SQL script/script.sql
@@ -0,0 +1,42 @@
+create table if not exists admins
+(
+    id       int auto_increment
+        primary key,
+    username varchar(20) not null,
+    password varchar(20) not null
+);
+
+create table if not exists shippers
+(
+    shipper_id     int auto_increment
+        primary key,
+    firstname      varchar(255) not null,
+    lastname       varchar(255) not null,
+    phone          varchar(20)  not null,
+    password       varchar(20)  not null,
+    birthDay       varchar(20)  not null,
+    email          varchar(255) not null,
+    address        varchar(255) null,
+    personal_image longblob     null,
+    constraint phone_UNIQUE
+        unique (phone)
+);
+
+create table if not exists orders
+(
+    order_id             int auto_increment
+        primary key,
+    shipper_id           int            null,
+    pickup_location      varchar(255)   not null,
+    delivery_location    varchar(255)   not null,
+    value                decimal(10, 2) not null,
+    other_details        text           null,
+    delivery_date_expect varchar(20)    null,
+    constraint orders_ibfk_1
+        foreign key (shipper_id) references shippers (shipper_id)
+);
+
+create index shipper_id
+    on orders (shipper_id);
+
+
